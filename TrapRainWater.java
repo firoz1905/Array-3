b316e4ee3d9c9a3b0ffc4fc829eb7bda71e1ae74
firoz Syed <syedfiroz2010@gmail.com>
2024-12-26 23:11:59 -0800

    one pass solution for TrapRainWater

--- TrapRainWater.java
+++ TrapRainWater.java
@@ -1,4 +1,5 @@
-// Approach : From brute force approach where you need to have lw and rw in order to trap. In one pass you can find
+// Approach : From brute force approach where you need to have lw and rw in order to trap. 
+// Using two pass approach with one pass you can find
 // the max height(tallest building) which acts as a rw for all the buildings.
 // so now you only need to have a lw and do the comparisons to see if you can tap the water
 // Time : O(n) + O(n)
@@ -44,6 +45,56 @@ class Solution {
                 // if we have a bigger right wall then we can trap water
                 result+=rw-height[r];
                 r--;
+            }
+        }
+        return result;
+    }
+}
+
+
+// Approach : From brute force approach where you need to have lw and rw in order to trap. 
+// with just one pass approach, at each and every building to trap the rain water if we know we have the
+// bigger wall at some where in the right than the currrent/left wall then we can trap the water based of left wall and vice versa.
+// so we dont need to know the tallest building before hand. so go with two pointers l & r and lw and rw.
+// Time : O(n) // one pass logic
+// Space : O(1)
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int l = 0;
+        int lw = 0; // left wall
+
+        int r = n - 1; // right wall
+        int rw = 0;
+
+        int result = 0; // trap
+
+        while (l <= r) {
+            // Before we compute , consider which wall is bigger
+            // if lw == rw we can choose to go either the left or right
+            if (lw <= rw) {
+                // implies we have bigger right wall
+                // Hence go to side of the left wall.
+                if (lw < height[l]) {
+                    lw = height[l];
+                    l++;
+                } else {
+                    // if we have a bigger left wall then we can trap water
+                    result += lw - height[l];
+                    l++;
+                }
+            } else {
+                // implies we have bigger left wall
+                // Hence go to side of the right wall.
+                if (rw < height[r]) {
+                    rw = height[r];
+                    r--;
+                } else {
+                    // if we have a bigger right wall then we can trap water
+                    result += rw - height[r];
+                    r--;
+                }
             }
         }
         return result;
